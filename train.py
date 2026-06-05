d9fb6ec82f91379b3b49034ab025c5f3c059ea15
Urho Lempiö <urho.lempio@tuni.fi>
2026-06-05 11:33:05 +0300

    add webdataset_loading

--- train.py
+++ train.py
@@ -5,247 +5,246 @@ from model import HOA_WavTokenizer
 from discriminator import DACDiscriminator, MultiPeriodDiscriminator, MultiResolutionDiscriminator
 from loss import MelSpecReconstructionLoss, GeneratorLoss, DiscriminatorLoss, FeatureMatchingLoss, DACGANLoss
 from dataloader import get_dataloaders
-from model import HOA_WavTokenizer
 from tqdm import tqdm
 
-device = "cuda" if torch.cuda.is_available() else "cpu"
+def main():
+    device = "cuda" if torch.cuda.is_available() else "cpu"
 
-# train_loader, val_loader = get_dataloaders(
-#     "./Volumes/My Book/hoa_out_speech/train",
-#     "./Volumes/My Book/hoa_out_speech/test"
-# )
+    train_loader, val_loader = get_dataloaders(
+        "/Volumes/MyBook/hoa_out_speech_shards/train",
+        "/Volumes/MyBook/hoa_out_speech_shards/test"
+    )
 
-train_loader, val_loader = get_dataloaders(
-    "./test_data/hoa_mixtures/hoa_mixtures_speech",
-    "./test_data/hoa_mixtures/hoa_mixtures_speech"
-)
+    # Model
+    model = HOA_WavTokenizer().to(device)
+    def count_parameters(model):
+        return sum(p.numel() for p in model.parameters())
+    total_params = count_parameters(model)
+    print(f"Model parameters: {total_params / 1e6:.2f}M") # 71.69M Model Parameters
 
-# Model
-model = HOA_WavTokenizer().to(device)
-def count_parameters(model):
-    return sum(p.numel() for p in model.parameters())
-total_params = count_parameters(model)
-print(f"Model parameters: {total_params / 1e6:.2f}M") # 71.69M Model Parameters
+    # Discriminators
+    disc_mpd = MultiPeriodDiscriminator().to(device)
+    disc_mrd = MultiResolutionDiscriminator().to(device)
+    disc_dac = DACDiscriminator().to(device)
+    discriminators = [disc_mpd, disc_mrd, disc_dac]
 
-# Discriminators
-disc_mpd = MultiPeriodDiscriminator().to(device)
-disc_mrd = MultiResolutionDiscriminator().to(device)
-disc_dac = DACDiscriminator().to(device)
-discriminators = [disc_mpd, disc_mrd, disc_dac]
+    # Losses 
+    mel_loss_fn = MelSpecReconstructionLoss(sample_rate=24000).to(device)
+    gen_loss_fn = GeneratorLoss().to(device)
+    disc_loss_fn = DiscriminatorLoss().to(device)
+    feat_match_loss_fn = FeatureMatchingLoss().to(device)
+    dac_loss = DACGANLoss(disc_dac).to(device)
 
-# Losses 
-mel_loss_fn = MelSpecReconstructionLoss(sample_rate=24000).to(device)
-gen_loss_fn = GeneratorLoss().to(device)
-disc_loss_fn = DiscriminatorLoss().to(device)
-feat_match_loss_fn = FeatureMatchingLoss().to(device)
-dac_loss = DACGANLoss(disc_dac).to(device)
+    # Optimizers
+    opt_gen = torch.optim.AdamW(model.parameters(), lr=2e-4)
+    disc_params = []
+    for d in discriminators:    
+        disc_params += list(d.parameters())
+    opt_disc = torch.optim.AdamW(disc_params, lr=2e-4)
 
-# Optimizers
-opt_gen = torch.optim.AdamW(model.parameters(), lr=2e-4)
-disc_params = []
-for d in discriminators:    
-    disc_params += list(d.parameters())
-opt_disc = torch.optim.AdamW(disc_params, lr=2e-4)
+    # Checkpoint loading
+    resume_path = "./checkpoints/checkpoint_latest.pt"
 
-# Checkpoint loading
-resume_path = "./checkpoints/checkpoint_latest.pt"
+    if os.path.exists(resume_path):
+        ckpt = torch.load(resume_path, map_location=device)
 
-if os.path.exists(resume_path):
-    ckpt = torch.load(resume_path, map_location=device)
+        model.load_state_dict(ckpt["model"])
+        disc_mpd.load_state_dict(ckpt["disc_mpd"])
+        disc_mrd.load_state_dict(ckpt["disc_mrd"])
+        disc_dac.load_state_dict(ckpt["disc_dac"])
 
-    model.load_state_dict(ckpt["model"])
-    disc_mpd.load_state_dict(ckpt["disc_mpd"])
-    disc_mrd.load_state_dict(ckpt["disc_mrd"])
-    disc_dac.load_state_dict(ckpt["disc_dac"])
+        opt_gen.load_state_dict(ckpt["opt_gen"])
+        opt_disc.load_state_dict(ckpt["opt_disc"])
 
-    opt_gen.load_state_dict(ckpt["opt_gen"])
-    opt_disc.load_state_dict(ckpt["opt_disc"])
+        global_step = ckpt["step"]
 
-    global_step = ckpt["step"]
+        print(f"✅ Resumed from step {global_step}")
 
-    print(f"✅ Resumed from step {global_step}")
-
-else:
-    global_step = 0
+    else:
+        global_step = 0
 
 
 
-max_steps = 2_001 
-pretrain_mel_steps = 0  # from config
-mel_loss_coeff = 45
-mrd_loss_coeff = 1.0
+    max_steps = 50_000
+    pretrain_mel_steps = 0  # from config
+    mel_loss_coeff = 45
+    mrd_loss_coeff = 1.0
 
-print("Starting training...")
+    print("Starting training...")
 
 
 
 
-pbar = tqdm(total=max_steps)
+    pbar = tqdm(total=max_steps)
+
+    batch = next(iter(train_loader))
+    print(f"batch['audio'].shape: {batch['audio'].shape}")  # Expecting [B, 1, T] B=batch size, 1=mono, T=number of samples
 
 
+    while global_step < max_steps:  
+        for batch in train_loader:
+            audio_input = batch["audio"].to(device)  # [B, 1, T]
+
+            # match Lightning behavior
+            train_discriminator = global_step >= pretrain_mel_steps
+
+            # ==================================================
+            # DISCRIMINATOR STEP
+            # ==================================================
+            if train_discriminator:
+                opt_disc.zero_grad()
+
+                with torch.no_grad():
+                    out = model(audio_input)
+                    audio_hat = out["audio"]
+
+                audio_input_1d = audio_input.squeeze(1)   # [B, 1, T] → [B, T]
+                audio_hat_1d  = audio_hat.squeeze(1)    # [B, 1, T] → [B, T]
+
+                # DAC discriminator loss
+                loss_dac = dac_loss.discriminator_loss(
+                    audio_hat, audio_input
+                )
+
+                # MPD
+                real_mp, gen_mp, _, _ = disc_mpd(
+                    y=audio_input_1d, y_hat=audio_hat_1d
+                )
+                loss_mp, loss_mp_real, _ = disc_loss_fn(
+                    disc_real_outputs=real_mp,
+                    disc_generated_outputs=gen_mp
+                )
+                loss_mp = loss_mp / len(loss_mp_real)
+
+                # MRD
+                real_mrd, gen_mrd, _, _ = disc_mrd(
+                    y=audio_input_1d, y_hat=audio_hat_1d
+                )
+                loss_mrd, loss_mrd_real, _ = disc_loss_fn(
+                    disc_real_outputs=real_mrd,
+                    disc_generated_outputs=gen_mrd
+                )
+                loss_mrd = loss_mrd / len(loss_mrd_real)
+
+                # total discriminator loss
+                loss_disc = loss_mp + mrd_loss_coeff * loss_mrd + loss_dac
+
+                loss_disc.backward()
+                opt_disc.step()
+
+            # ==================================================
+            # GENERATOR STEP
+            # ==================================================
+            opt_gen.zero_grad()
+
+            out = model(audio_input, bandwidth=6.6)
+            audio_hat = out["audio"]
+            commit_loss = out["commit_loss"]
+
+            if train_discriminator:
+                # DAC generator loss
+                loss_dac_1, loss_dac_2 = dac_loss.generator_loss(
+                    audio_hat,
+                    audio_input
+                )
+
+                # MPD
+                _, gen_mp, fmap_rs_mp, fmap_gs_mp = disc_mpd(
+                    y=audio_input_1d, y_hat=audio_hat_1d
+                )
+
+                loss_gen_mp, list_loss_gen_mp = gen_loss_fn(gen_mp)
+                loss_gen_mp = loss_gen_mp / len(list_loss_gen_mp)
+
+                loss_fm_mp = feat_match_loss_fn(
+                    fmap_r=fmap_rs_mp,
+                    fmap_g=fmap_gs_mp
+                ) / len(fmap_rs_mp)
+
+                # MRD
+                _, gen_mrd, fmap_rs_mrd, fmap_gs_mrd = disc_mrd(
+                    y=audio_input_1d, y_hat=audio_hat_1d
+                )
+
+                loss_gen_mrd, list_loss_gen_mrd = gen_loss_fn(gen_mrd)
+                loss_gen_mrd = loss_gen_mrd / len(list_loss_gen_mrd)
+
+                loss_fm_mrd = feat_match_loss_fn(
+                    fmap_r=fmap_rs_mrd,
+                    fmap_g=fmap_gs_mrd
+                ) / len(fmap_rs_mrd)
+
+            else:
+                # pretraining phase
+                loss_gen_mp = 0
+                loss_gen_mrd = 0
+                loss_fm_mp = 0
+                loss_fm_mrd = 0
+                loss_dac_1 = 0
+                loss_dac_2 = 0
 
 
-while global_step < max_steps:  
-    for batch in train_loader:
-        audio_input = batch["audio"].to(device)  # [B, 1, T]
+            # Mel loss
+            mel_loss = mel_loss_fn(audio_hat, audio_input)
 
-        # match Lightning behavior
-        train_discriminator = global_step >= pretrain_mel_steps
-
-        # ==================================================
-        # DISCRIMINATOR STEP
-        # ==================================================
-        if train_discriminator:
-            opt_disc.zero_grad()
-
-            with torch.no_grad():
-                out = model(audio_input)
-                audio_hat = out["audio"]
-
-            audio_input_1d = audio_input.squeeze(1)   # [B, 1, T] → [B, T]
-            audio_hat_1d  = audio_hat.squeeze(1)    # [B, 1, T] → [B, T]
-
-            # DAC discriminator loss
-            loss_dac = dac_loss.discriminator_loss(
-                audio_hat, audio_input
+            # total generator loss
+            loss_gen = (
+                loss_gen_mp
+                + mrd_loss_coeff * loss_gen_mrd
+                + loss_fm_mp
+                + mrd_loss_coeff * loss_fm_mrd
+                + mel_loss_coeff * mel_loss
+                + 1000 * commit_loss
+                + loss_dac_1
+                + loss_dac_2
             )
 
-            # MPD
-            real_mp, gen_mp, _, _ = disc_mpd(
-                y=audio_input_1d, y_hat=audio_hat_1d
-            )
-            loss_mp, loss_mp_real, _ = disc_loss_fn(
-                disc_real_outputs=real_mp,
-                disc_generated_outputs=gen_mp
-            )
-            loss_mp = loss_mp / len(loss_mp_real)
+            loss_gen.backward()
+            opt_gen.step()
 
-            # MRD
-            real_mrd, gen_mrd, _, _ = disc_mrd(
-                y=audio_input_1d, y_hat=audio_hat_1d
-            )
-            loss_mrd, loss_mrd_real, _ = disc_loss_fn(
-                disc_real_outputs=real_mrd,
-                disc_generated_outputs=gen_mrd
-            )
-            loss_mrd = loss_mrd / len(loss_mrd_real)
+            # ==================================================
+            # LOGGING & CHECKPOINTS
+            # ==================================================
+            if global_step % 10 == 0:
+                print(
+                    f"[{global_step}] "
+                    f"G: {loss_gen.item():.4f} | "
+                    f"D: {loss_disc.item() if train_discriminator else 0:.4f} | "
+                    f"Mel: {mel_loss.item():.4f} | "
+                    f"Commit: {commit_loss.item():.6f}"
+                )
+            if global_step % 1000 == 0:
+                checkpoint = {
+                    "model": model.state_dict(),
+                    "disc_mpd": disc_mpd.state_dict(),
+                    "disc_mrd": disc_mrd.state_dict(),
+                    "disc_dac": disc_dac.state_dict(),
+                    "opt_gen": opt_gen.state_dict(),
+                    "opt_disc": opt_disc.state_dict(),
+                    "step": global_step,
+                }
 
-            # total discriminator loss
-            loss_disc = loss_mp + mrd_loss_coeff * loss_mrd + loss_dac
+                torch.save(checkpoint, f"./checkpoints/checkpoint_{global_step}.pt")
+                print(f"✅ Saved checkpoint at step {global_step}")
+            if global_step % 2000 == 0:
+                torchaudio.save(
+                    f"./generator_samples/sample_{global_step}.wav",
+                    audio_hat[0].detach().cpu(),
+                    24000
+                )
+            global_step += 1
 
-            loss_disc.backward()
-            opt_disc.step()
+            # ==================================================
+            # PROGRESS BAR UPDATE
+            # ==================================================
+            pbar.update(1)
 
-        # ==================================================
-        # GENERATOR STEP
-        # ==================================================
-        opt_gen.zero_grad()
+            if global_step % 100 == 0:
+                pbar.set_description(
+                    f"G:{loss_gen.item():.2f} D:{loss_disc.item():.2f}"
+                )
 
-        out = model(audio_input, bandwidth=6.6)
-        audio_hat = out["audio"]
-        commit_loss = out["commit_loss"]
+    print("Training completed successfully!")
 
-        if train_discriminator:
-            # DAC generator loss
-            loss_dac_1, loss_dac_2 = dac_loss.generator_loss(
-                audio_hat,
-                audio_input
-            )
-
-            # MPD
-            _, gen_mp, fmap_rs_mp, fmap_gs_mp = disc_mpd(
-                y=audio_input_1d, y_hat=audio_hat_1d
-            )
-
-            loss_gen_mp, list_loss_gen_mp = gen_loss_fn(gen_mp)
-            loss_gen_mp = loss_gen_mp / len(list_loss_gen_mp)
-
-            loss_fm_mp = feat_match_loss_fn(
-                fmap_r=fmap_rs_mp,
-                fmap_g=fmap_gs_mp
-            ) / len(fmap_rs_mp)
-
-            # MRD
-            _, gen_mrd, fmap_rs_mrd, fmap_gs_mrd = disc_mrd(
-                y=audio_input_1d, y_hat=audio_hat_1d
-            )
-
-            loss_gen_mrd, list_loss_gen_mrd = gen_loss_fn(gen_mrd)
-            loss_gen_mrd = loss_gen_mrd / len(list_loss_gen_mrd)
-
-            loss_fm_mrd = feat_match_loss_fn(
-                fmap_r=fmap_rs_mrd,
-                fmap_g=fmap_gs_mrd
-            ) / len(fmap_rs_mrd)
-
-        else:
-            # pretraining phase
-            loss_gen_mp = 0
-            loss_gen_mrd = 0
-            loss_fm_mp = 0
-            loss_fm_mrd = 0
-            loss_dac_1 = 0
-            loss_dac_2 = 0
-
-
-        # Mel loss
-        mel_loss = mel_loss_fn(audio_hat, audio_input)
-
-        # total generator loss
-        loss_gen = (
-            loss_gen_mp
-            + mrd_loss_coeff * loss_gen_mrd
-            + loss_fm_mp
-            + mrd_loss_coeff * loss_fm_mrd
-            + mel_loss_coeff * mel_loss
-            + 1000 * commit_loss
-            + loss_dac_1
-            + loss_dac_2
-        )
-
-        loss_gen.backward()
-        opt_gen.step()
-
-        # ==================================================
-        # LOGGING & CHECKPOINTS
-        # ==================================================
-        if global_step % 10 == 0:
-            print(
-                f"[{global_step}] "
-                f"G: {loss_gen.item():.4f} | "
-                f"D: {loss_disc.item() if train_discriminator else 0:.4f} | "
-                f"Mel: {mel_loss.item():.4f} | "
-                f"Commit: {commit_loss.item():.6f}"
-            )
-        if global_step % 1000 == 0:
-            checkpoint = {
-                "model": model.state_dict(),
-                "disc_mpd": disc_mpd.state_dict(),
-                "disc_mrd": disc_mrd.state_dict(),
-                "disc_dac": disc_dac.state_dict(),
-                "opt_gen": opt_gen.state_dict(),
-                "opt_disc": opt_disc.state_dict(),
-                "step": global_step,
-            }
-
-            torch.save(checkpoint, f"./checkpoints/checkpoint_{global_step}.pt")
-            print(f"✅ Saved checkpoint at step {global_step}")
-        if global_step % 2000 == 0:
-            torchaudio.save(
-                f"./generator_samples/sample_{global_step}.wav",
-                audio_hat[0].detach().cpu(),
-                24000
-            )
-        global_step += 1
-
-        # ==================================================
-        # PROGRESS BAR UPDATE
-        # ==================================================
-        pbar.update(1)
-
-        if global_step % 100 == 0:
-            pbar.set_description(
-                f"G:{loss_gen.item():.2f} D:{loss_disc.item():.2f}"
-            )
-
-print("Training completed successfully!")
+if __name__ == "__main__":
+    main()
