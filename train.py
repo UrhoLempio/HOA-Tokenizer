ee419bbd4beee6022f555f376fcf6106a5347e60
Urho Lempiö <urho.lempio@tuni.fi>
2026-07-16 14:12:22 +0300

    fix bug in generator step

--- train.py
+++ train.py
@@ -334,19 +334,18 @@ def main(config):
                 commit_loss = out["commit_loss"]
             #print("audio_input.shape =", audio_input.shape)
             #print("audio_hat.shape   =", audio_hat.shape)
-
-            if global_step % 10 == 0:
-                print(
-                    f"CommitRaw: {commit_loss.item():.3e} | "
-                    f"AudioHatMax: {audio_hat.abs().max().item():.3e}",
-                    flush=True,
-                )
-                if not torch.isfinite(commit_loss):
-                    print(f"BAD COMMIT LOSS at step {global_step}")
-                    break
-                if not torch.isfinite(audio_hat).all():
-                    print(f"BAD AUDIO_HAT at step {global_step}")
-                    break
+                if global_step % 10 == 0:
+                    print(
+                        f"CommitRaw: {commit_loss.item():.3e} | "
+                        f"AudioHatMax: {audio_hat.abs().max().item():.3e}",
+                        flush=True,
+                    )
+                    if not torch.isfinite(commit_loss):
+                        print(f"BAD COMMIT LOSS at step {global_step}")
+                        break
+                    if not torch.isfinite(audio_hat).all():
+                        print(f"BAD AUDIO_HAT at step {global_step}")
+                        break
 
                 if train_discriminator:
                     loss_dac_1_total = 0.0
