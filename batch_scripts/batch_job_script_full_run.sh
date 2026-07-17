4fb5fee7cd9b684f520bf05c06dd696f978f1adb
Urho Lempiö <urho.lempio@tuni.fi>
2026-07-17 09:31:40 +0300

    increase batch job mem

--- batch_scripts/batch_job_script_full_run.sh
+++ batch_scripts/batch_job_script_full_run.sh
@@ -5,7 +5,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=10
-#SBATCH --mem=32G
+#SBATCH --mem=64G
 #SBATCH --time=3-00:00:00
 #SBATCH --gres=gpu:v100:1,nvme:10
 #SBATCH --output=slurm-%j.out
