e4b7003f1e77c7723bd289d45e8dc5a9cda24953
Urho Lempiö <urho.lempio@tuni.fi>
2026-07-18 11:34:34 +0300

    bug fix dot_item

--- train.py
+++ train.py
@@ -15,6 +15,7 @@ from torch.utils.tensorboard import SummaryWriter
 from torch.amp import autocast, GradScaler
 
 
+
 # TQDM switch since cluster terminals may not support it. Use config to set this.
 USE_TQDM = False
 
@@ -474,8 +475,8 @@ def main(config):
                 writer.add_scalar("loss/train_disc", loss_disc.item(), global_step)
                 writer.add_scalar("loss/mel", mel_loss.item(), global_step)
                 writer.add_scalar("loss/commit", commit_loss.item(), global_step)
-                writer.add_scalar("loss/gen_mp", loss_gen_mp, global_step)
-                writer.add_scalar("loss/gen_mrd", loss_gen_mrd, global_step)
+                writer.add_scalar("loss/gen_mp", loss_gen_mp.item(), global_step)
+                writer.add_scalar("loss/gen_mrd", loss_gen_mrd.item(), global_step)
              
             if spatial_loss_coeff != 0.0 and (spatial_loss_every <= 1 or global_step % spatial_loss_every == 0):
                 writer.add_scalar("loss/spatial", spatial_loss.item(), global_step)
