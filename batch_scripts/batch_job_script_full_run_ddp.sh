f0952e18275b2e2a8d01853dddf0979c479d9f83
Urho Lempiö <urho.lempio@tuni.fi>
2026-08-11 07:22:19 +0300

    update config and batch job script

--- batch_scripts/batch_job_script_full_run_ddp.sh
+++ batch_scripts/batch_job_script_full_run_ddp.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
-#SBATCH --job-name=foa_test_run_50k_steps
+#SBATCH --job-name=foa_test_run
 #SBATCH --account=project_2013256
-#SBATCH --partition=gpu
-#SBATCH --nodes=1
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=10
-#SBATCH --mem=64G
-#SBATCH --time=3-00:00:00
-#SBATCH --gres=gpu:v100:4,nvme:10
+#SBATCH --partition=gpumedium
+#SBATCH --time=00:30:00
+#SBATCH --nodes=4
+#SBATCH --ntasks-per-node=4 --cpus-per-task=72  # The product should be 288
+#SBATCH --gres=gpu:gh200:4  # 4 GPUs per node
 #SBATCH --output=slurm-%j.out
 #SBATCH --error=slurm-%j.err
 
 module purge
-module load pytorch/2.6
+module load pytorch/2.10
 
 source /projappl/project_2013256/lempio/hoa_env1/bin/activate
 
-export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+# Set the number of CPU threads based on cpus-per-task
+export OMP_NUM_THREADS=${SLURM_CPUS_PER_TASK:-1}
 
 echo "Running on $(hostname)"
 nvidia-smi
 
 # Run training (UNBUFFERED!)
-srun python3 -u train.py configs/train_cluster_foa.yaml
+srun PYTHONUNBUFFERED=1 torchrun --standalone --nnodes=1 --nproc_per_node=4 train.py configs/train_cluster_foa.yaml
