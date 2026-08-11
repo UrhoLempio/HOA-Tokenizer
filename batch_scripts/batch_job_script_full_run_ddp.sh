8e26f6edf0f5502f04adb29260dc21ffd380a023
Urho Lempiö <urho.lempio@tuni.fi>
2026-08-11 07:34:31 +0300

    update batch job script

--- batch_scripts/batch_job_script_full_run_ddp.sh
+++ batch_scripts/batch_job_script_full_run_ddp.sh
@@ -3,7 +3,7 @@
 #SBATCH --account=project_2013256
 #SBATCH --partition=gpumedium
 #SBATCH --time=00:30:00
-#SBATCH --nodes=4
+#SBATCH --nodes=1
 #SBATCH --ntasks-per-node=4 --cpus-per-task=72  # The product should be 288
 #SBATCH --gres=gpu:gh200:4  # 4 GPUs per node
 #SBATCH --output=slurm-%j.out
