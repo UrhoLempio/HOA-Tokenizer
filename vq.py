e2f73bed4ea754be329bdeabef2795d6fb69dfb3
Urho Lempiö <urho.lempio@tuni.fi>
2026-09-04 10:28:10 +0300

    add ema reset wit hgrace count

--- vq.py
+++ vq.py
@@ -260,6 +260,12 @@ class EuclideanCodebook(nn.Module):
         )
         self.embed.data.copy_(modified_codebook)
 
+        grace_count = max(10.0, float(self.threshold_ema_dead_code + 1))
+        self.cluster_size.data.masked_fill_(mask, grace_count)
+        self.embed_avg.data[mask] = (
+            self.embed.data[mask] * self.cluster_size.data[mask].unsqueeze(1)
+        )
+
     def expire_codes_(self, batch_samples):
         if self.threshold_ema_dead_code == 0:
             return
@@ -329,27 +335,27 @@ class EuclideanCodebook(nn.Module):
     def forward(self, x):
         shape, dtype = x.shape, x.dtype
         x = self.preprocess(x)
-
         self.init_embed_(x)
-
         embed_ind = self.quantize(x)
         embed_onehot = F.one_hot(embed_ind, self.codebook_size).type(dtype)
         embed_ind = self.postprocess_emb(embed_ind, shape)
         quantize = self.dequantize(embed_ind)
-
         if self.training:
             # We do the expiry of code at that point as buffers are in sync
             # and all the workers will take the same decision.
-            self.expire_codes_(x)
-            ema_inplace(self.cluster_size, embed_onehot.sum(0), self.decay)
+            # updates codebook (embed)
+            self.expire_codes_(x) 
+            # updates cluster_size
+            ema_inplace(self.cluster_size, embed_onehot.sum(0), self.decay) 
             embed_sum = x.t() @ embed_onehot
-            ema_inplace(self.embed_avg, embed_sum.t(), self.decay)
+            # updates embed_avg
+            ema_inplace(self.embed_avg, embed_sum.t(), self.decay) 
             cluster_size = (
                 laplace_smoothing(self.cluster_size, self.codebook_size, self.epsilon)
                 * self.cluster_size.sum()
             )
             embed_normalized = self.embed_avg / cluster_size.unsqueeze(1)
-            self.embed.data.copy_(embed_normalized)
+            self.embed.data.copy_(embed_normalized) 
 
         return quantize, embed_ind
 
